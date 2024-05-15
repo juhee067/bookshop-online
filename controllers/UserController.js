3f8670eade3dda7ae6c5cdcd8c47e673f99c1cb4
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-15 16:27:13 +0900

    feat:users controller 추가

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,19 @@
 const db = require('../models');
 
 const User = db.Users;
-console.log(User);
+const addUser = async (req, res) => {
+  let info = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    address: req.body.address,
+    contact: req.body.contact,
+  };
+
+  const user = await User.create(info).catch((err) => console.log(err));
+  res.status(200).send(user);
+};
+
+module.exports = {
+  addUser,
+};
