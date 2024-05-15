e8fbe738e93ea21b09ab58b839e0efbe4aa3d262
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-15 23:04:13 +0900

    feat:signin email 확인

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,7 +21,7 @@ const signin = async (req, res) => {
     if (!user) {
       return res.status(401).json({ status: 401 });
     }
-    res.status(200).json({ status: 200 });
+    res.status(200).json({ status: 200, user: user });
   } catch (err) {
     console.error('로그인 중 오류 발생:', err);
     res.status(500).json({ message: '서버 오류 발생' });
