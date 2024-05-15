231c74f8c4c8919f4dae4d06ca0a5e080523ce96
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-15 23:09:03 +0900

    feat:userService - generateToken - hashpPwd 추가

--- services/userService.js
+++ services/userService.js
@@ -1,3 +1,14 @@
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
-const userService = {};
+const userService = {
+  generateToken: (id, email, name) => {
+    return jwt.sign({ id, email, name }, process.env.PRIVATE_KEY, {
+      expiresIn: '1h',
+      issuer: 'juhee',
+    });
+  },
+  hashPwd: (res, salt) => {
+    const { password } = req.body;
+    return crypto.pbkdf2Sync(password, salt, 10000, 10, 'sha512').toString('base64');
+  },
+};
