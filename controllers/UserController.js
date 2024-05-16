02edb9856e6872889c81aa055b78bb73a0635f97
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-16 23:07:38 +0900

    #2 refactor:user등록 및 비번 암호화 service 파일로 이동

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,22 +1,10 @@
-const db = require('../models');
 const userService = require('../services/userService');
-const crypto = require('crypto');
 
-const User = db.users;
 const signup = async (req, res) => {
   const user = req.body;
-  const salt = crypto.randomBytes(10).toString('base64');
-  const hashPwd = userService.hashPwd(user.password, salt);
-  let userData = {
-    username: user.username,
-    email: user.email,
-    password: hashPwd,
-    address: user.address,
-    contact: user.contact,
-    salt: salt,
-  };
+
   try {
-    await User.create(userData);
+    await userService.createUser(user);
   } catch (err) {
     console.log(err);
     return res.status(400).json({ status: 400 });
@@ -26,18 +14,15 @@ const signup = async (req, res) => {
 };
 
 const signin = async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ where: { email } });
-  const hashPwd = userService.hashPwd(password, user.salt);
-
   try {
-    const userEmail = await User.findOne({ where: { email } });
+    const { email, password } = req.body;
+    const user = await userService.findUserByEmail(email);
+    const hashPwd = userService.hashPwd(password, user.salt);
 
-    if (!userEmail || user.password !== hashPwd) {
+    if (!user || user.password !== hashPwd) {
       return res.status(401).json({ status: 401 });
     }
-    res.status(200).json({ status: 200, user: userEmail });
+    res.status(200).json({ status: 200, user: user });
   } catch (err) {
     console.error('로그인 중 오류 발생:', err);
     res.status(500).json({ message: '서버 오류 발생' });
--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,17 @@
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const db = require('../models');
+const User = db.users;
 const userService = {
+  createUser: async (user) => {
+    const salt = crypto.randomBytes(10).toString('base64');
+    const hashPwd = userService.hashPwd(user.password, salt);
+    return await User.create({ ...user, password: hashPwd, salt: salt });
+  },
+  findUserByEmail: async (email) => {
+    return await User.findOne({ where: { email } });
+  },
+
   generateToken: (id, email, name) => {
     return jwt.sign({ id, email, name }, process.env.PRIVATE_KEY, {
       expiresIn: '1h',
